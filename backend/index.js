f084d10da960e9c8205cc48a5be0510eeffaf63b
spburtsev <spburtsev@proton.me>
2026-03-16 13:10:12 +0100

    feat(backend): use

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,18 @@
-const express = require('express')
+import express from 'express'
+import { serve, setup } from 'swagger-ui-express'
+import { readFileSync } from 'fs'
+import { load } from 'js-yaml'
+
+const swaggerDocument = load(readFileSync('./openapi.yaml', 'utf8'))
+
 const app = express()
 const port = 3001
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+
+app.use('/docs', serve, setup(swaggerDocument))
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
